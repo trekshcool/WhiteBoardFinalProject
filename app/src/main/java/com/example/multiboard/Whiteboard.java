b0a5d4c5889d57fccae74453282ed3b6219b1fa3
wblucca <wblucca@wpi.edu>
2020-02-16 14:20:09 -0500

    Whiteboard list and TODO for geofencing

--- app/src/main/java/com/example/multiboard/Whiteboard.java
+++ app/src/main/java/com/example/multiboard/Whiteboard.java
@@ -6,11 +6,13 @@ package com.example.multiboard;
  */
 public class Whiteboard {
 
-    private final static int width = 10000;
-    private final static int height = 10000;
+    private final static int width = 5000;
+    private final static int height = 5000;
 
     private String name; // Unique name representing this Whiteboard
     private Pixel[][] board = new Pixel[width][height]; // The board data itself
+
+    // TODO Whiteboard GPS location and GeoFence structure, getters and setters
 
     public Whiteboard(String name) {
         this.name = name;
