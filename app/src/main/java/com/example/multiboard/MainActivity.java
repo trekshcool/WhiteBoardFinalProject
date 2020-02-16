83c1280b31b3a1c6bd08a8a5d6e62d137f6c8f48
wblucca <wblucca@wpi.edu>
2020-02-16 18:04:52 -0500

    Sign in works in SignInActivity (no longer Main)

--- app/src/main/java/com/example/multiboard/MainActivity.java
+++ app/src/main/java/com/example/multiboard/MainActivity.java
@@ -2,20 +2,50 @@ package com.example.multiboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private List<Whiteboard> whiteboardList;
+    private static final String TAG = "MainActivity";
+
+    // Firebase variables
+    private final String ANONYMOUS = "Anonymous";
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private DatabaseReference mFirebaseDatabaseReference;
+
+    // User information
+    private String mUsername;
+
+    private List<Whiteboard> mWhiteboardList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        whiteboardList = new ArrayList<>();
+        mWhiteboardList = new ArrayList<>();
+
+        // Set default username is anonymous.
+        mUsername = ANONYMOUS;
+
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        if (mFirebaseUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+        } else {
+            mUsername = mFirebaseUser.getDisplayName();
+        }
     }
 }
