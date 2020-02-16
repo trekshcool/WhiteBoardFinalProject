90079c10c8a4c9e1e667e81a0bb7c5206bcb3b6d
Srlucas <StephenLucas600@gmail.com>
2020-02-16 14:38:15 -0500

    Preview GPS Locations for buildings

---
+++ app/src/main/java/com/example/multiboard/GeoLocations.java
@@ -0,0 +1,10 @@
+package com.example.multiboard;
+
+public interface GeoLocations {
+
+}
+    // source: https://www.mapdevelopers.com/draw-circle-tool.php?circles=%5B%5B29.88%2C42.2750396%2C-71.8063774%2C%22%23AAAAAA%22%2C%22%23000000%22%2C0.4%5D%5D
+    // Fuller labs: 42.275040,-71.806377, 34.94
+    // Atwater Kent: 42.275262,-71.807008 Radius: 37.83 Meters
+    //Kaven Hall: Position: 42.274915,-71.805822 Radius: 29.00 Meters
+    //George C/ Cordon Libary:  Position: 42.274230,-71.806369 Radius: 32.53 Meters
