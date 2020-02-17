54f14f53a68431f4ed094ca205fe4f3469b9e570
wblucca <wblucca@wpi.edu>
2020-02-17 08:32:29 -0500

    Remove salting of usernames, no longer needed

--- app/src/main/java/com/example/multiboard/MainActivity.java
+++ app/src/main/java/com/example/multiboard/MainActivity.java
@@ -26,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     // Shared Preferences variables
-    private final String SALT_KEY = "SALT_KEY";
     private SharedPreferences mSharedPreferences;
     private String sharedPrefFile = "com.example.multiboard";
 
@@ -36,10 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private DatabaseReference mFirebaseDatabaseReference;
 
     // User information
-    private final int SALT_LENGTH = 64;
-    private final String SALT_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     private int mUserId;
-    private String mUserSalt;
 
     // GUI variables
     LinearLayout linearWhiteboards;
@@ -53,15 +49,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // SharedPreferences initialization/restoration
-        // Generate salt for username if none exists, store in shared preferences
         mSharedPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
-        mUserSalt = mSharedPreferences.getString(SALT_KEY, genSaltString());
-        SharedPreferences.Editor preferencesEditor = mSharedPreferences.edit();
-        preferencesEditor.putString(SALT_KEY, mUserSalt);
-        preferencesEditor.apply();
 
         // Initialize user ID
-        mUserId = mUserSalt.hashCode();
+        mUserId = 0;
 
         // Initialize Firebase Auth
         mFirebaseAuth = FirebaseAuth.getInstance();
@@ -71,13 +62,11 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(this, SignInActivity.class));
             finish();
         } else {
-            // Anonymize user data by salting and hashing the name before storing it
+            // Anonymize user data by hashing the name to create an ID number before storing it
             String dispName = mFirebaseUser.getDisplayName();
             if (dispName != null) {
-                String saltedName = dispName + mUserSalt;
-                mUserId = saltedName.hashCode();
+                mUserId = dispName.hashCode();
                 Log.d(TAG, dispName);
-                Log.d(TAG, saltedName);
                 Log.d(TAG, Integer.toString(mUserId));
             }
         }
@@ -107,24 +96,5 @@ public class MainActivity extends AppCompatActivity {
             View cardView = inflater.inflate(R.layout.whiteboard_list_card, linearWhiteboards);
             mWhiteboardMap.put(wb, cardView);
         }
-    }
-
-    /**
-     * Generate a salt to modify the user's display name uniquely.
-     * @return a random string of length SALT_LENGTH using the SALT_ALPHABET.
-     */
-    private String genSaltString() {
-        Random r = new Random();
-        StringBuilder sb = new StringBuilder();
-
-        // Select SALT_LENGTH characters
-        for (int i = 0; i < SALT_LENGTH; i++) {
-            // Pick one letter in SALT_ALPHABET
-            char randChar = SALT_ALPHABET.charAt(r.nextInt(SALT_ALPHABET.length()));
-            sb.append(randChar);
-        }
-
-        // Build the string and return
-        return sb.toString();
     }
 }
