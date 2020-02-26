e337b1cb47b477ef7664b8ef80c3158728a2fe60
Srlucas <StephenLucas600@gmail.com>
2020-02-26 15:59:43 -0500

    start pupup menue

--- app/src/main/java/com/example/multiboard/PaintPopUp.java
+++ app/src/main/java/com/example/multiboard/PaintPopUp.java
@@ -2,11 +2,20 @@ package com.example.multiboard;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 class PaintPopUp extends Activity {
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.popup);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        int width = displayMetrics.widthPixels;
+        int height = displayMetrics.heightPixels;
+
+        getWindow().setLayout(width, height);
     }
 }
